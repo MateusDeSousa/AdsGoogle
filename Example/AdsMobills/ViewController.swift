87e4d33e33d463f3dabf7edc4d08cd6299560ff9
Mateus Sousa <mateusdevsousa@gmail.com>
2019-10-22 18:01:53 -0300

    imprementação do google ads

--- Example/AdsMobills/ViewController.swift
+++ Example/AdsMobills/ViewController.swift
@@ -7,18 +7,38 @@
 //
 
 import UIKit
+import AdsMobills
+import GoogleMobileAds
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GADInterstitialDelegate {
 
+    var interstitial: GADInterstitial!
+//    var googleAds: AdsMobillsInterstitial!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        interstitial.delegate = self
+        let request = GADRequest()
+        interstitial.load(request)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func onclick(_ sender: Any) {
+        
+        if interstitial.isReady {
+          interstitial.present(fromRootViewController: self)
+        }
+    }
+    
+    func interstitialWillDismissScreen(_ ad: GADInterstitial) {
+        interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+        let request = GADRequest()
+        interstitial.load(request)
+    }
+    
 }
 
