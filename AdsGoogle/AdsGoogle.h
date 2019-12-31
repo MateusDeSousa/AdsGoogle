6a91b4cc4f43be1bb90e02d6fae4b8125b7e575d
Mateus Sousa <mateusdevsousa@gmail.com>
2019-12-31 13:10:47 -0300

    Initial project setup

---
+++ AdsGoogle/AdsGoogle.h
@@ -0,0 +1,19 @@
+//
+//  AdsGoogle.h
+//  AdsGoogle
+//
+//  Created by Mateus Sousa on 31/12/19.
+//  Copyright © 2019 Mateus Sousa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdsGoogle.
+FOUNDATION_EXPORT double AdsGoogleVersionNumber;
+
+//! Project version string for AdsGoogle.
+FOUNDATION_EXPORT const unsigned char AdsGoogleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdsGoogle/PublicHeader.h>
+
+
---
+++ AdsGoogleExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdsGoogleExamples
+//
+//  Created by Mateus Sousa on 31/12/19.
+//  Copyright © 2019 Mateus Sousa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
